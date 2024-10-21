dec114954d86ca42c873b1936f6313afc4f99c95
aidleovil <aidaleon@hotmail.es>
2024-10-21 09:15:16 +0200

    commit Ejercicio 10 con enunciado

--- src/Tema2/ejercicio_10.java
+++ src/Tema2/ejercicio_10.java
@@ -1,5 +1,6 @@
 package Tema2;
-
+/*Realiza un programa que indique si los 3 números que pedimos por
+teclado están ordenados de menor a mayor o no.*/
 public class ejercicio_10 {
     public static void main(String[]args){
 
