243d431e7e0339792ba67ea1923775a8430eee76
aidleovil <aidaleon@hotmail.es>
2024-10-28 10:11:24 +0100

    hecho ejercicio 2 y 3

--- src/Tema3/ejercicio_2.java
+++ src/Tema3/ejercicio_2.java
@@ -5,29 +5,36 @@ import java.util.Scanner;
 /*Realiza un programa que lee por consola dos números enteros (A y B).
 Si B es mayor que A, muestra todos los números impares entre ellos*/
 public class ejercicio_2 {
-    public static void main(String[]args){
-        Scanner in= new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         System.out.println("Dime dos números enteros a tu elección");
-        int num_1= in.nextInt();
-        int num_2= in.nextInt();
-        while (num_1>num_2){
+        int num_1 = in.nextInt();
+        int num_2 = in.nextInt();
+        while (num_1 > num_2) {
             System.out.println("Tu primer número es mayor al segundo. " +
                     "Si quieres los numeros impares ingresa el número mayor en segundo lugar ");
-            num_1= in.nextInt();
-            num_2= in.nextInt();
+            num_1 = in.nextInt();
+            num_2 = in.nextInt();
         }
-
-        for (num_1; num_1>num_2; num_1=num_1+2 ){
-
+        if (num_1==num_2 && num_2%2!=0){
+            System.out.println(num_1);
+            if (num_2%2==0){
+                System.out.println("No hay impares");
+            }
         }
-        while (num_1 <= num_2) {
-                if (num_1 % 2 != 0) {
-                    System.out.println(num_1);
-                    num_1 = num_1 + 2;
-                } else {
-                    System.out.println("No tienes impares.");
-                }
+        if (num_1%2!=0){
+        System.out.println("Tus números impares son ");
+            while (num_2>=num_1) {
+            System.out.print(num_1 + " ");
+            num_1 = num_1 + 2;
+            }
+        }else {
+            System.out.println("Tus números impares son ");
+            while (num_2 >= num_1 && num_1 % 2 == 0) {
+                num_1++;
+                System.out.print(num_1 + " ");
+                num_1++;
+            }
         }
     }
 }
-
---
+++ src/Tema3/ejercicio_3.java
@@ -0,0 +1,14 @@
+package Tema3;
+/* Escribe un programa que calcula la suma de todos los números pares
+entre 1 y 1000 y la muestra por consola.*/
+public class ejercicio_3 {
+    public static void main(String[]args) {
+        int num = 2;
+        int num_2 = 0;
+        while (num > 1 && num < 1001) {
+            num_2 = num_2 + num;
+            num = num + 2;
+        }
+        System.out.println("Tus suma da " + num_2);
+    }
+}
