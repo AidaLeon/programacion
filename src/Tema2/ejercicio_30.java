4223ba304672d850a28be0c24c3143d04438683a
aidaleonvila <aidaleon@hotmail.es>
2024-11-07 19:40:52 +0100

    acabado el ejercicio 30. Empezado el juego, solo si jugador uno tiene mas velocidad.

---
+++ src/Tema2/ejercicio_30.java
@@ -0,0 +1,100 @@
+package Tema2;
+
+import java.util.Scanner;
+
+/*Haz una calculaoora. La calculadora permitirá hacer varias operaciones. Pedirá un número real, pedirá una
+operación (+, -, *, / o %), pedirá otro número real y mostrará el resultado por consola. A continuación pedirá
+los datos para una nueva operación. Piensa en un modo para que se pueda salir de la calculadora en cualquier momento.*/
+public class ejercicio_30 {
+    public static void main(String[]args){
+        Scanner in= new Scanner(System.in);
+        boolean seguir=true;
+        System.out.println("Dime el número que desees");
+        double num_1= in.nextDouble();
+        System.out.println("Dime el calculo que desees hacer (+ , - , * , / o %)");
+        char simbolo= in.next().charAt(0);
+                /*String conversor=in.nextLine();*/
+        System.out.println("Dime el número que desees");
+        double num_2= in.nextDouble();
+        while (simbolo=='+'&& seguir==true){
+            System.out.println(num_1 + " " + simbolo + " " + num_2 + " = " + (num_1+num_2));
+            System.out.println("Si quieres seguir pon + si quieres salir pon -");
+            char salir=in.next().charAt(0);
+            if (salir=='-'){
+                seguir=false;
+            }else {
+                System.out.println("Dime el número que desees");
+                num_1= in.nextDouble();
+                System.out.println("Dime el calculo que desees hacer (+ , - , * , / o %)");
+                simbolo= in.next().charAt(0);
+                System.out.println("Dime el número que desees");
+                num_2= in.nextDouble();
+            }
+        }
+        while (simbolo=='-' && seguir==true){
+            System.out.println(num_1 + " " + simbolo + " " + num_2 + " = " + (num_1-num_2));
+            System.out.println("Si quieres seguir pon + si quieres salir pon -");
+            char salir=in.next().charAt(0);
+            if (salir=='-'){
+                seguir=false;
+            }else {
+                System.out.println("Dime el número que desees");
+                num_1= in.nextDouble();
+                System.out.println("Dime el calculo que desees hacer (+ , - , * , / o %)");
+                simbolo= in.next().charAt(0);
+                System.out.println("Dime el número que desees");
+                num_2= in.nextDouble();
+            }
+        }
+        while (simbolo=='*' && seguir==true){
+            System.out.println(num_1 + " " + simbolo + " " + num_2 + " = " + (num_1*num_2));
+            System.out.println("Si quieres seguir pon + si quieres salir pon -");
+            char salir=in.next().charAt(0);
+            if (salir=='-'){
+                seguir=false;
+            }else {
+                System.out.println("Dime el número que desees");
+                num_1= in.nextDouble();
+                System.out.println("Dime el calculo que desees hacer (+ , - , * , / o %)");
+                simbolo= in.next().charAt(0);
+                System.out.println("Dime el número que desees");
+                num_2= in.nextDouble();
+            }
+        }
+        while (simbolo=='/' && seguir==true){
+            System.out.println(num_1 + " " + simbolo + " " + num_2 + " = " + (num_1/num_2));
+            System.out.println("Si quieres seguir pon + si quieres salir pon -");
+            char salir=in.next().charAt(0);
+            if (salir=='-'){
+                seguir=false;
+            }else {
+                System.out.println("Dime el número que desees");
+                num_1= in.nextDouble();
+                System.out.println("Dime el calculo que desees hacer (+ , - , * , / o %)");
+                simbolo= in.next().charAt(0);
+                System.out.println("Dime el número que desees");
+                num_2= in.nextDouble();
+            }
+        }
+        while (simbolo=='%' && seguir==true){
+            double potencia=num_1%num_2;
+            if (potencia<0){
+                potencia=potencia+num_2;
+            }
+            System.out.println(num_1 + " " + simbolo + " " + num_2 + " = " + potencia );
+            System.out.println("Si quieres seguir pon + si quieres salir pon -");
+            char salir=in.next().charAt(0);
+            if (salir=='-'){
+                seguir=false;
+            }else {
+                System.out.println("Dime el número que desees");
+                num_1= in.nextDouble();
+                System.out.println("Dime el calculo que desees hacer (+ , - , * , / o %)");
+                simbolo= in.next().charAt(0);
+                System.out.println("Dime el número que desees");
+                num_2= in.nextDouble();
+            }
+        }
+        System.out.println("¡Adios!");
+    }
+}
---
+++ src/Tema2/juego.java
@@ -0,0 +1,165 @@
+package Tema2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+Realiza una aplicación que simula un combate entre dos jugadores. Primero leerá los valores de los
+dos jugadores y a continuación empezará a simular un combate por turnos.*/
+public class juego {
+    public static void main(String[]args) {
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
+        int velocidad_1;
+        int velocidad_2;
+        int ataque_1;
+        int ataque_2;
+        int defensa_1;
+        int defensa_2;
+        int puntosvida_1;
+        int puntosvida_2;
+        int puntostotal_1;
+        int puntostotal_2;
+
+        System.out.println("Dime los valores de los atributos, del jugador 1. Recuerda que el rango de los atributos es de 200," +
+                "y la suma de todos ellos no puede superar 500");
+        System.out.println("¿Velocidad cuanto quieres?");
+        velocidad_1 = in.nextInt();
+        System.out.println("¿Ataque cuanto quieres?");
+        ataque_1 = in.nextInt();
+        System.out.println("¿Defensa cuanto quieres?");
+        defensa_1 = in.nextInt();
+        System.out.println("¿Puntos de vida cuanto quieres?");
+        puntosvida_1 = in.nextInt();
+        puntostotal_1 = velocidad_1 + ataque_1 + defensa_1 + puntosvida_1;
+        while (velocidad_1 > 200 || ataque_1 > 200 || defensa_1 > 200 || puntosvida_1 > 200 || puntostotal_1 > 500) {
+            System.out.println("Lo siento tus valores no son validos. Repite los valores de los atributos, del jugador 1. Recuerda que el rango de los atributos es de 200," +
+                    "y la suma de todos ellos no puede superar 500");
+            System.out.println("¿Velocidad cuanto quieres?");
+            velocidad_1 = in.nextInt();
+            System.out.println("¿Ataque cuanto quieres?");
+            ataque_1 = in.nextInt();
+            System.out.println("¿Defensa cuanto quieres?");
+            defensa_1 = in.nextInt();
+            System.out.println("¿Puntos de vida cuanto quieres?");
+            puntosvida_1 = in.nextInt();
+            puntostotal_1 = velocidad_1 + ataque_1 + defensa_1 + puntosvida_1;
+        }
+        System.out.println("Dime los valores de los atributos, del jugador 2. Recuerda que el rango de los atributos es de 200," +
+                "y la suma de todos ellos no puede superar 500");
+        System.out.println("¿Velocidad cuanto quieres?");
+        velocidad_2 = in.nextInt();
+        System.out.println("¿Ataque cuanto quieres?");
+        ataque_2 = in.nextInt();
+        System.out.println("¿Defensa cuanto quieres?");
+        defensa_2 = in.nextInt();
+        System.out.println("¿Puntos de vida cuanto quieres?");
+        puntosvida_2 = in.nextInt();
+        puntostotal_2 = velocidad_2 + ataque_2 + defensa_2 + puntosvida_2;
+        while (velocidad_2 > 200 || ataque_2 > 200 || defensa_2 > 200 || puntosvida_2 > 200 || puntostotal_2 > 500) {
+            System.out.println("Lo siento tus valores no son validos. Repite los valores de los atributos, del jugador 2. Recuerda que el rango de los atributos es de 200," +
+                    "y la suma de todos ellos no puede superar 500");
+            System.out.println("¿Velocidad cuanto quieres?");
+            velocidad_2 = in.nextInt();
+            System.out.println("¿Ataque cuanto quieres?");
+            ataque_2 = in.nextInt();
+            System.out.println("¿Defensa cuanto quieres?");
+            defensa_2 = in.nextInt();
+            System.out.println("¿Puntos de vida cuanto quieres?");
+            puntosvida_2 = in.nextInt();
+            puntostotal_2 = velocidad_1 + ataque_1 + defensa_1 + puntosvida_1;
+        }
+        int ronda = 1;
+        System.out.println("*******************************");
+        System.out.println("Ronda " + ronda);
+        System.out.println("Jugador 1: " + puntosvida_1 + "---------------------------------");
+        System.out.println("Jugador 2: " + puntosvida_2 + "---------------------------------");
+        while (puntosvida_1 > 0 && puntosvida_2 > 0) {
+            ronda++;
+            if (velocidad_1 >= velocidad_2) {
+                System.out.println("Jugar 1, es tu turno.Si desees atacar apreta 1, si deseas defenderte apreta 2, si deseas" +
+                        " hacer un crítico (probabilidad de causar un ataque que genera el doble de daño) apreta 3");
+                int jugada_1 = in.nextInt();
+                if (jugada_1 == 1) {
+                    if (ataque_1 < 50) {
+                        int dano = random.nextInt(10);
+                        System.out.println("Le has hecho " + dano + " puntos de daño");
+                        puntostotal_2 = puntosvida_2 - dano;
+                    } else {
+                        int dano = 10 + random.nextInt(20);
+                        System.out.println("Le has hecho " + dano + " puntos de daño");
+                        puntostotal_2 = puntosvida_2 - dano;
+                    }
+                } else if (jugada_1 == 2) {
+                    if (defensa_1 < 50) {
+                        int vida = random.nextInt(10);
+                        System.out.println("Te has dado " + vida + " puntos de vida.");
+                        puntostotal_1 = puntosvida_1 + vida;
+                    } else {
+                        int vida = 5 + random.nextInt(20);
+                        System.out.println("Te has dado " + vida + " puntos de vida.");
+                        puntostotal_1 = puntosvida_1 + vida;
+                    }
+                } else {
+                    if (ataque_1 < 50) {
+                        int danox2 = random.nextInt(20);
+                        System.out.println("Le has hecho " + danox2 + " puntos de daño");
+                        puntostotal_2 = puntosvida_2 - danox2;
+                    } else {
+                        int danox2 = 10 + random.nextInt(40);
+                        System.out.println("Le has hecho " + danox2 + " puntos de daño");
+                        puntostotal_2 = puntosvida_2 - danox2;
+                    }
+                }
+                if (puntostotal_2 > 0) {
+                    System.out.println("Jugar 2, es tu turno.Si desees atacar apreta 1, si deseas defenderte apreta 2, si deseas" +
+                            " hacer un crítico (probabilidad de causar un ataque que genera el doble de daño) apreta 3");
+                    int jugada_2 = in.nextInt();
+                    if (jugada_2 == 1) {
+                        if (ataque_2 < 50) {
+                            int dano = random.nextInt(10);
+                            System.out.println("Le has hecho " + dano + " puntos de daño");
+                            puntostotal_1 = puntosvida_1 - dano;
+                        } else {
+                            int dano = 10 + random.nextInt(20);
+                            System.out.println("Le has hecho " + dano + " puntos de daño");
+                            puntostotal_1 = puntosvida_1 - dano;
+                        }
+                    } else if (jugada_2 == 2) {
+                        if (defensa_1 < 50) {
+                            int vida = random.nextInt(10);
+                            System.out.println("Te has dado " + vida + " puntos de vida.");
+                            puntostotal_2 = puntosvida_2 + vida;
+                        } else {
+                            int vida = 5 + random.nextInt(20);
+                            System.out.println("Te has dado " + vida + " puntos de vida.");
+                            puntostotal_2 = puntosvida_2 + vida;
+                        }
+                    } else {
+                        if (ataque_2 < 50) {
+                            int danox2 = random.nextInt(20);
+                            System.out.println("Le has hecho " + danox2 + " puntos de daño");
+                            puntostotal_1 = puntosvida_1 - danox2;
+                        } else {
+                            int danox2 = 10 + random.nextInt(40);
+                            System.out.println("Le has hecho " + danox2 + " puntos de daño");
+                            puntostotal_1 = puntosvida_1 - danox2;
+                        }
+                    }
+                } else {
+                    System.out.println("Ha ganado el jugador 1");
+                    break;
+                }
+                puntosvida_1 = puntostotal_1;
+                puntosvida_2 = puntostotal_2;
+                System.out.println("*******************************");
+                System.out.println("Ronda " + ronda);
+                System.out.println("Jugador 1: " + puntostotal_1 + "---------------------------------");
+                System.out.println("Jugador 2: " + puntostotal_2 + "---------------------------------");
+            }
+        }
+        if (puntostotal_1 < 0) {
+            System.out.println("Ha ganado el jugador 2.");
+        }
+    }
+}
