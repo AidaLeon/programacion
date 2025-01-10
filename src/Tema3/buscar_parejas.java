5cabcda4a2b101b19babe832ca3ee359f101bab5
aidleovil <aidaleon@hotmail.es>
2025-01-10 13:12:09 +0100

    parejas mierda

--- src/Tema3/buscar_parejas.java
+++ src/Tema3/buscar_parejas.java
@@ -1,10 +1,45 @@
 package Tema3;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class buscar_parejas {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-       int[]array1=new int[20];
+        int[] noVisible = new int[20];
+        array(noVisible);
+        noVisible=array(noVisible);
+        System.out.println(Arrays.toString(noVisible));
+    }
+
+    public static int[] array(int[] noVisible) {
+
+        int[] posicionesOcupadas = new int[20];
+        System.out.println(Arrays.toString(posicionesOcupadas));
+        Random random = new Random();
+        int posicion;
+        int posicion2;
+        for (int animal = 1; animal < 11; animal++) {
+            do {
+                posicion = random.nextInt(0, 21);
+                do {
+                    posicion2 = random.nextInt(0,21);
+                } while (posicion2==posicion);
+
+            } while (esta(posicionesOcupadas, posicion) || esta(posicionesOcupadas,posicion2));
+            posicionesOcupadas[2*animal-1]=posicion;
+            posicionesOcupadas[2*animal-2]=posicion2;
+            noVisible[posicion-1]=animal;
+            noVisible[posicion2-1]=animal;
+        }
+        return noVisible;
+    }
+    public static boolean esta(int[] num, int posicion) {
+        for (int i = 0; i < num.length; i++) {
+            if (posicion==num[i]){
+                return true;
+            }
+        }
+        return false;
     }
 }
