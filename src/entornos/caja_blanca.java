52a1aac546a535fc245c6fb1ca270c2d052f9616
aidleovil <aidaleon@hotmail.es>
2025-01-14 13:59:16 +0100

    entornos hecho

--- src/entornos/caja_blanca.java
+++ src/entornos/caja_blanca.java
@@ -1,17 +1,26 @@
 package entornos;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class caja_blanca {
     public static void main(String[] args) {
-        char[] cadena= {'a', 'b', 'c' , 'd'};
-        char letra= 'c';
+        Scanner in=new Scanner(System.in);
+        char [] ejercicio1=new char[1];
+        System.out.println("introduce la cadena con 1 letra");
+        char[] cadena=new char[1];
+        for (int i = 0; i < cadena.length; i++) {
+            ejercicio1[i]=in.nextLine().charAt(0);
+
+        }
+        System.out.println("Dime la letra que quieras");
+        char letra=in.nextLine().charAt(0);
+        System.out.println("Tu cadena " + Arrays.toString(ejercicio1) + " contiene el caracter " + letra + " " +contar_letras(ejercicio1, letra) + " veces");
     }
-    public static int contar_letras(char cadena[], char letra){
+    public static int contar_letras(char[] cadena, char letra){
         int contador=0;
         int n=0;
-        int lon=4;
-        /*int lon=strlen(cadena);*/
+        int lon=cadena.length;
         if (lon>0){
             do {
                 if (cadena[contador]==letra){
@@ -22,37 +31,6 @@ public class caja_blanca {
             }while (lon>0);
         }
         return n;
-    }
-    public static int ejercicio2(int campo1, int campo2, int registros){
-        int contador=0;
-
-
-        do {
-            if (campo1==0){
-            contador++;
-            } else if (campo2==0) {
-                contador=0;
-            }else {
-
-            }
-        }while (registros>0);
-        return contador;
-    }
-    public static int ejercicio3(){
-        Scanner in=new Scanner(System.in);
-        int contador=0;
-        do {
-            System.out.println("dime el numero del campo");
-            int campo1= in.nextInt();
-            if (campo1==0){
-                contador++;
-            } else if (campo1==1) {
-                contador=0;
-            }else {
-
-            }
-        }while (contador<5);
-        return contador;
     }
 
 }
