dc1d5d3677d20785ba95ca8cbe0ae54462e8524e
aidleovil <aidaleon@hotmail.es>
2025-01-16 14:07:07 +0100

    empezado router

--- src/Tema3/cobertura_matriz.java
+++ src/Tema3/cobertura_matriz.java
@@ -8,9 +8,19 @@ public class cobertura_matriz {
         int [][] hotel=new int[12][5];
         System.out.println("Dime el numero de la planta del 1 al 12");
         int planta= in.nextInt()-1;
-        System.out.println("Dime el numero de la planta del 1 al 12");
+        System.out.println("Dime el numero de la habiatación del 1 al 5");
         int habitacion= in.nextInt()-1;
 
+    }
+    public static int comprobacionPlanta(int planta, int habitacion){
+        while (planta>12 || planta<0){
+            System.out.println("Tu valor no es correcto recuerda es del 1 al 12");
+
+        }
+        while(habitacion>5 || habitacion<0){
+
+        }
+        return planta;
     }
 
 
