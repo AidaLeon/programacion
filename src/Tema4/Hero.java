956bf7be1cbd4f01af4565f8ca1279b3041ada73
aidaleonvila <aidaleon@hotmail.es>
2025-01-28 19:48:33 +0100

    formula videojuego

--- src/Tema4/Hero.java
+++ src/Tema4/Hero.java
@@ -1,5 +1,9 @@
 package Tema4;
 
+import java.util.Random;
+
+import static java.lang.Math.random;
+
 public class Hero {
     private String nombre;
     private int level, health, maxHealth, experience, attack, defense;
@@ -26,7 +30,7 @@ public class Hero {
         return nombre;
     }
     public void setNombre(String nombre){
-        if (nombre.length()>20 || nombre.length()<1){
+        if (nombre.length()>20 || nombre.length()<0){
             System.out.println("Tu nombre no es valido");
         }else {
             this.nombre=nombre;
@@ -49,20 +53,91 @@ public class Hero {
     public int getHealth() {
         return health;
     }
+    public void setHealth(int health){
+        if (health<1 || health>100){
+            System.out.println("Tus puntos de vida no son correctos");
+        }else {
+            this.health=health;
+        }
+    }
 
     public int getMaxHealth() {
         return maxHealth;
     }
+    public void setMaxHealth(int maxHealth){
+        if (maxHealth>120 || maxHealth<1){
+            System.out.println("Tus puntos de máximo de vida son incorrectos");
+        }else {
+            this.maxHealth=maxHealth;
+        }
+    }
 
-    public int getExperience() {
+    public int getExperience(int experiencia) {
         return experience;
+    }
+    public void setExperience(int experience){
+        if (experience<1||experience>100){
+            System.out.println("Tus puntos de experiencia no son válidos");
+        }
+        else {
+            this.experience=experience;
+        }
     }
 
     public int getAttack() {
         return attack;
     }
+    public void setAttack(int attack){
+        if (attack<1||attack>100){
+            System.out.println("Tus puntos de daño no son correctos");
+        }else {
+            this.attack=attack;
+        }
+    }
 
     public int getDefense() {
         return defense;
     }
+    public void setDefense(int defense){
+        if (defense<1||defense>100){
+            System.out.println("Tus puntos de defensa no son validos");
+        }else {
+            this.defense=defense;
+        }
+    }
+    public static int drinkPotion(int health){
+        health=+10;
+        if (health>100){
+            health=100;
+        }
+        return health;
+    }
+    public static int rest(int health){
+        health=+50;
+        if (health>100){
+            health=100;
+        }
+
+        return health;
+    }
+    public static void mostrar(String nombre, int level, int health, int maxHealth, int experience, int attack, int defense){
+        System.out.println(nombre + "\n"+
+                "Nivel -> "+level + "\n"+
+                "Vida -> "+health + "\n"+
+                "Máximo de vida -> "+maxHealth + "\n"+
+                "Experiencia -> "+experience + "\n"+
+                "Ataque -> "+attack + "\n"+
+                "Defensa ->"+defense + "\n");
+    }
+    public static int attack(int health, int attack, int defense){
+        Random random=new Random();
+        int dano;
+        dano=random.nextInt(attack-defense);
+        health=-dano;
+        if (health<0){
+            health=0;
+        }
+        return health;
+    }
+
 }
--- src/Tema4/Hero_Programa.java
+++ src/Tema4/Hero_Programa.java
@@ -4,18 +4,37 @@ import java.util.Scanner;
 
 public class Hero_Programa {
     public static void main(String[] args) {
-        Scanner in= new Scanner(System.in);
-        Hero hero1=new Hero();
+        Scanner in = new Scanner(System.in);
+        Hero hero1 = new Hero();
 
         System.out.println("Dime el nombre de tu heroe");
-        String nombre=in.nextLine().toUpperCase();
+        String nombre = in.nextLine().toUpperCase();
+        hero1.setNombre(nombre);
 
         System.out.println("Dime el nivel que quieras del 1 al 10");
-        int nivel=in.nextInt();
+        int level = in.nextInt();
+        hero1.setLevel(level);
 
-        System.out.println("Dime la vid que quieras del 1 al 100");
-        int vida=in.nextInt();
+        System.out.println("Dime la vida que quieras del 1 al 100");
+        int health = in.nextInt();
+        hero1.setHealth(health);
 
+        System.out.println("Dime el máximo de vida que puede tener del 1 al 120");
+        int maxHealth = in.nextInt();
+        hero1.setMaxHealth(maxHealth);
 
+        System.out.println("Dime la cantidad de experiencia que tiene del 1 al 100");
+        int experience = in.nextInt();
+        hero1.getExperience(experience);
+
+        System.out.println("Dime el ataque del 1 al 100");
+        int attack = in.nextInt();
+        hero1.setAttack(attack);
+
+        System.out.println("Dime la defensa del 1 al 100");
+        int defense = in.nextInt();
+        hero1.setDefense(defense);
+
+        Hero.mostrar(nombre, level, health, maxHealth, experience, attack, defense);
     }
 }
