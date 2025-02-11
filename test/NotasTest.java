8de5d4bb49d49d369b4348f4cfc695f261026b73
aidaleonvila <aidaleon@hotmail.es>
2025-02-11 18:19:52 +0100

    Entornos

---
+++ test/NotasTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class NotasTest {
+  @Test
+  public void testInsuficiente(){
+    assertEquals("Insuficiente", Notas.opciones(3));
+  }
+  @Test
+  public void testSuficiente(){
+    assertEquals("Suficiente", Notas.opciones(5.5));
+  }
+  @Test
+  public void testNotable(){
+    assertEquals("Notable", Notas.opciones(7.5));
+  }
+  @Test
+  public void testSobresaliente(){
+    assertEquals("Sobresaliente", Notas.opciones(10));
+  }
+
+}
