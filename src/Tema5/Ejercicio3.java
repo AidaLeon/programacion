36c739dd9a97aeb945669efbbdc21d10b9f209a2
aidleovil <aidaleon@hotmail.es>
2025-02-14 14:01:08 +0100

    acabar ejercicios

--- src/Tema5/Ejercicio3.java
+++ src/Tema5/Ejercicio3.java
@@ -5,24 +5,27 @@ import java.util.*;
 public class Ejercicio3 {
     public static void main(String[] args) {
         Scanner in= new Scanner(System.in);
-        List<Double>nota =new Vector<>();
+        Vector<Double> nota =new Vector<>(5);
 
-        int largo=0;
         double numero=0;
 
-        System.out.println("Dime un numero");
-        for (int i = 0; i < nota.size(); i++) {
 
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Dime el numero: "+i);
             try {
 
                 numero = in.nextDouble();
 
 
             } catch (InputMismatchException e) {
+                System.out.println("Input");
                 System.out.println("Valor no correcto");
+                System.out.println(e.getMessage());
+                in.nextLine();
+            }finally {
+                nota.add(numero);
 
             }
-            nota.add(numero);
         }
         System.out.println(nota);
 
